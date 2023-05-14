9facb8ac892dc4d7dcc58b6990b1f4f493fef793
Youssef_Elfeky <youssefelfeky83@gmail.com>
2023-05-14 19:07:20 +0300

    update

--- Healthbar.py
+++ Healthbar.py
@@ -4,29 +4,42 @@ from OpenGL.GLUT import *
 
 WINDOW_WIDTH = 1200
 WINDOW_HEIGHT = 700
-def draw_white_bar():
+
+'''
+To draw the health bar we have to do 3 steps:
+1- We draw the white bar which represents full health.
+2- We need to calculate the length of the green bar which represnts health.
+3- We change the color of the health bar.
+4- We draw the green bar.
+'''
+
+
+# 1- drawing the white bar on the top left of the screen relative to the car center.
+def draw_white_bar(center):
     glColor3f(1, 1, 1)
-    glPushMatrix()
     glBegin(GL_POLYGON)
-    glVertex(28, WINDOW_HEIGHT-30, 0)
-    glVertex(182, WINDOW_HEIGHT-30, 0)
-    glVertex(182, WINDOW_HEIGHT-10, 0)
-    glVertex(28, WINDOW_HEIGHT-10, 0)
+    glVertex(center[0] - 60 - 230, center[1] - 7 + 165, 0)
+    glVertex(center[0] + 60 - 230, center[1] - 7 + 165, 0)
+    glVertex(center[0] + 60 - 230, center[1] + 7 + 165, 0)
+    glVertex(center[0] - 60 - 230, center[1] + 7 + 165, 0)
     glEnd()
-    glPopMatrix()
 
-def draw_health_bar(health):
-    glColor3f(1 - health/100, health/100, 0)
-    Green_Section = (health * (150/100)) + 30
-    glPushMatrix()
+def draw_health_bar(health, center):
+    # 2- as health decreases the green degree decreases and the red degree increases.
+    glColor3f(1 - health / 100, health / 100, 0)
+    # 3- drawing the bar depends mainly on health as (Green_Section / 116 = health / 100)
+    Green_Section = (health * (116 / 100))
     glBegin(GL_POLYGON)
-    glVertex(30, WINDOW_HEIGHT - 28, 0)
-    glVertex(Green_Section, WINDOW_HEIGHT - 28, 0)
-    glVertex(Green_Section, WINDOW_HEIGHT - 12, 0)
-    glVertex(30, WINDOW_HEIGHT - 12, 0)
+    # 4- fixing the left side of the health to decrease only from right.
+    glVertex(center[0] - 58 - 230, center[1] - 5 + 165, 0)
+    glVertex(center[0] - 58 + Green_Section - 230, center[1] - 5 + 165, 0)
+    glVertex(center[0] - 58 + Green_Section - 230, center[1] + 5 + 165, 0)
+    glVertex(center[0] - 58 - 230, center[1] + 5 + 165, 0)
     glEnd()
-    glPopMatrix()
 
-def draw_health(health):
-    draw_health_bar(health)
-    draw_white_bar()
+
+def draw_health(health, center):
+    glPushMatrix()
+    draw_health_bar(health, center)
+    draw_white_bar(center)
+    glPopMatrix()
